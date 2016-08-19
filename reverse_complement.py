fe20ae82644546aafc7ead658e9696b6d615eacc
JuneBS <june.sallou@laposte.net>
2016-08-19 21:44:26 +0200

    docstring

--- reverse_complement.py
+++ reverse_complement.py
@@ -1,19 +1,28 @@
 #! /usr/bin/python3.4
 # -*-coding:utf-8 -*
 
-"""
-Codes done thanks to the Coursera course named Biology Meets Programming: Bioinformatics for Beginners
-
-"""
+""" Codes done thanks to the Coursera course named Biology Meets Programming: Bioinformatics for Beginners """
 
 """ Module reverse_complement that return the reverse complement of a DNA sequence"""
 
 def complement(Nucleotide):
     """
         Returns the complement sequence of the input sequence
+	
+	:param: the sequence made of the letters A,C,G,T
+	:type: string
+	:return: the complement sequence
+	:rtype: string
+
+	:Example:
+
+	>>> complement('ATTGC')
+	'TAACG'
+
+	..warnings:: The input sequence absolutely needs to only contain the letters A, C, T and G. No other letter is allowed.
     """
+
     comp = '' # output variable
-    # your code here
     Nucleotide=str(Nucleotide)
     for i in Nucleotide:
         if i=='A':
@@ -26,13 +35,26 @@ def complement(Nucleotide):
             comp=comp + 'C'
     return comp
 
-def ReverseComplement(Pattern):
+
+def reverseComplement(Pattern):
     """
         Returns the reverse complement sequence of the input sequence.
-        Calls the complement() function.
+        
+	:param: The sequence made of the letters A,C,G,T
+	:type: string
+	:return: the reverse complement of the sequence
+	:rtype: string
+
+	:Example:
+
+	>>> reverseComplement('ATGCT')
+	'AGCAT' 
+
+	..seealso:: complement()
+	.. warnings:: The input sequence absolutely needs to only contain the letters A, C, T and G. No other letter is allowed.
     """
+
     revComp = '' # output variable
-    # your code here
     comp=complement(Pattern)
     for i in comp:
         revComp = str(i) + revComp
